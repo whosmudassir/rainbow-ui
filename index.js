dcc2cfe005915eb0f319a2d5fd097cf6674d3cda
whosmudassir <deadsoulartist@gmail.com>
2022-03-30 16:15:18 +0530

    fix: hamburger menu fix

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const triggerHamburger = document.querySelector("#trigger-hamburger");
+const triggerSlider = document.querySelector("#trigger-slider");
+
+triggerHamburger.onclick = function () {
+  if (triggerSlider.className == "list-box-open") {
+    triggerSlider.className = "list-box-close";
+  } else {
+    triggerSlider.className = "list-box-open";
+  }
+};
