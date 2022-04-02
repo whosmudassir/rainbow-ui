981d966998aebde0587f28d3ea23c4085e65bdf1
whosmudassir <deadsoulartist@gmail.com>
2022-04-02 16:43:45 +0530

    feat: dark theme logic with css

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ const renderThemeIcon = document.querySelector("#render-theme-icon");
 triggerTheme.onclick = function () {
   if (renderThemeIcon.className == "fa fa-sun") {
     renderThemeIcon.className = "fa fa-moon";
+    document.body.classList.remove("dark-theme");
   } else if (renderThemeIcon.className == "fa fa-moon") {
     renderThemeIcon.className = "fa fa-sun";
+    document.body.classList.toggle("dark-theme");
   }
 };
